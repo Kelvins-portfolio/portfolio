fd72e05697d093cf20e5bd8aa0bb44ebebb36ed1
Kelvins-portfolio <elinkbiashara@outlook.com>
2024-08-19 17:06:31 +0300

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { FaLinkedin } from "react-icons/fa6";
+import { IoLogoGithub } from "react-icons/io";
+import { HiOutlineDocumentDownload } from "react-icons/hi";
+
+export default function Home() {
+  return (
+    <main>  
+      <div class="contain "> 
+        <div class="card-container "> 
+          <div class="descripcion title-font"> 
+            <h1 className="text-4xl">John Smith</h1> 
+            <h2>Web Developer</h2> 
+            <p className="leading-5">
+              Hi there! I'm John, a seasoned web developer passionate about crafting clean and dynamic websites. With 2 years of experience, I specialize in front-end and back-end development using HTML, CSS, JavaScript, SEO, Mongodb and frameworks like React and Nodejs.
+
+              I thrive on creating user-centric designs and optimizing performance for seamless browsing experiences.
+
+              Let's collaborate TOGHETER!
+            </p> 
+            <div class="icons"> 
+              <a href="https://www.linkedin.com/in/#" class="hover-text" target="_blank"> 
+                <span class="tooltip-text" id="fade"> LinkedIn</span> 
+                <FaLinkedin />
+              </a> 
+              <a href="https://github.com/Mburu-Waa-Waanjiku" class="hover-text" target="_blank"> 
+                <span class="tooltip-text" id="fade"> GitHub </span> 
+                <IoLogoGithub /> 
+              </a> 
+              <a href="/KELVIN-MBURU-PORTFOLIO.pdf" class="hover-text" target="_blank"> <span class="tooltip-text" id="fade"> Resume </span> 
+                <HiOutlineDocumentDownload/> 
+              </a> 
+            </div> 
+          </div> 
+          <div class="image " style={{minWidth: 260}}> 
+            <Image src="/profile2.webp" width={200} height={200} alt=""/> 
+          </div> 
+        </div> 
+      </div>  
+    </main> 
+  );
+}
