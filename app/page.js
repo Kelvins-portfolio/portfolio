7b3295209a4f8f65efe5f8975b959765d2a430fb
Kelvins-portfolio <elinkbiashara@outlook.com>
2024-08-19 17:25:44 +0300

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <div class="contain "> 
         <div class="card-container "> 
           <div class="descripcion title-font"> 
-            <h1 className="text-4xl">John Smith</h1> 
+            <h1 className="text-3xl">Kelvin Mburu</h1> 
             <h2>Web Developer</h2> 
             <p className="leading-5">
               Hi there! I'm John, a seasoned web developer passionate about crafting clean and dynamic websites. With 2 years of experience, I specialize in front-end and back-end development using HTML, CSS, JavaScript, SEO, Mongodb and frameworks like React and Nodejs.
@@ -33,7 +33,7 @@ export default function Home() {
             </div> 
           </div> 
           <div class="image " style={{minWidth: 260}}> 
-            <Image src="/profile2.webp" width={200} height={200} alt=""/> 
+            <Image src="/profile2.jpg" width={200} height={200} alt=""/> 
           </div> 
         </div> 
       </div>  
